44a593f33d5e7ad9a2bb677b37e5e8e6b8bf22dd
Abhay Korat <abhaykorat89@gmail.com>
2026-05-16 17:32:48 +0530

    first commit

---
+++ src/scripts/js/index.js
@@ -0,0 +1,10 @@
+import toggleNavbarHandler from "./toggleNavbarHandler.js";
+import activeNavLinkHandler from "./activeNavLinkHandler.js";
+import scrollToDestinationHandler from "./scrollToDestinationHandler.js";
+import animateOnScroll from "./animateOnScroll.js";
+toggleNavbarHandler();
+activeNavLinkHandler();
+scrollToDestinationHandler();
+window.addEventListener('DOMContentLoaded', () => animateOnScroll());
+window.addEventListener('scroll', () => animateOnScroll());
+window.addEventListener('resize', () => animateOnScroll());
